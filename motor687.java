2b74a894553ea474ee197eb22b39d4796ba3dd17
Fleal2004 <45887816+Fleal2004@users.noreply.github.com>
2019-01-11 14:29:54 -0800

    Update motor687.java

--- motor687.java
+++ motor687.java
@@ -12,27 +12,25 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.subsystems.ExampleSubsystem;
 
 public class Robot extends TimedRobot {
   public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
   public static OI m_oi;
 
-  
-  private SpeedController m_flywheel;
+  private final TalonSRX m_flywheel;
   private Joystick m_joystick;
-  private Timer timer;
-  Command m_autonomousCommand;
+  
 
   public void robotInit() {
     m_oi = new OI();
       SmartDashboard.putData("Auto mode", m_chooser);
     m_flywheel = new Talon(kMotorPort);
     m_joystick = new Joystick(kJoystickPort);
-    
-    }
-    m_flywheel.set(-m_joystick.getY());
   }
+
+  
+       m_flywheel.set(-m_joystick.getY());
+
 
   public void goPercent(int percent, float time)
   {
@@ -42,7 +40,4 @@ public class Robot extends TimedRobot {
     m_flywheel.set(percent);
   }
 
-  public void stop(){
-    m_flywheel.set(0);
-  }
 }
